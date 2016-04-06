9db9c18eb6d2e707359f7e5fb6b69dbb4f7802cd
huomeiting <meiting_huo@hotmail.com>
2016-04-06 13:16:57 +0000

    half of the db.py

---
+++ LICENSE/db.py
@@ -0,0 +1,24 @@
+#/usr/bin/python
+#FileName:db.py
+class _Engine(object):
+	def __init__(self,connect):
+		self._connect = connect
+	def connect(self):
+		return self._connect()
+
+engine = None
+
+class _DbCtx(threading.local):
+	def __init__(self):
+		self.connection = None
+		self.transactions = 0
+
+	def is_init(self):
+		return not self.connection is None
+
+	def init(self):
+		self.connection = _LasyConnection()
+		self.transations = 0
+
+	def cleanup(self):
+		
